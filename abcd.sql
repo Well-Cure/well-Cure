03a4719ac8fbe2aebeeabea10e4ac6ecf5138787
W2_80455 <91207383+W2-80455@users.noreply.github.com>
2024-01-03 16:05:17 +0530

    SQl-ER

--- abcd.sql
+++ abcd.sql
@@ -14,14 +14,11 @@ create table Users(
   Login_date_time datetime
 );
 
+insert into Users (USRId,UName,Mobile,Email,Role,password,Login_date_time) values(1,"jitu@123","7999842948","jitendra@gmail.com","Patient","Jitendra@123",curdate());
+insert into Users (USRId,UName,Mobile,Email,Role,password,Login_date_time) values(2,"lokesh@123","7222842948","lokesh@gmail.com","Doctor","lokesh@123",curdate());
+insert into Users (UName,Mobile,Email,Role,password,Login_date_time) values("dhanraj@123","7111842948","dhanraj@gmail.com","Admin","dhanraj@123",curdate());
+select * from Users;
 desc Users;
-
-INSERT INTO Users(UName, Mobile, Email, Role,password) VALUES('Vipin','9876543210','vipin@gmail.com','Patient','vipin');
-
-INSERT INTO Users(UName, Mobile, Email, Role,password) VALUES('Aniket','9876543210','aniket@gmail.com','Doctor','aniket'),
-('Aditya','9876543210','aditya@gmail.com','Patient','aditya'),('Akash','9876543210','akash@gmail.com','Doctor','akash');
-
-
 
 create table Patients(
   PtntId int not null primary key auto_increment ,
@@ -34,17 +31,19 @@ create table Patients(
   DOB date not null,
   Home_Phone varchar(10),
   Emrgncy_phone varchar(10),
-  Emrgncy_mail varchar(35),
+  Emrgncy_email varchar(35),
   Address LongText not null,
   State varchar(30),
   City varchar(30),
   Pincode integer(6) not null,
   foreign key(UsrId) references Users(UsrId) on delete cascade on update cascade
 );
+alter table Patients modify UsrId int not null;
+insert into Patients (UsrId,First_name,Last_name,Gender,Ptnt_Weight,Ptnt_Height,DOB,Home_Phone,Emrgncy_phone,Emrgncy_email,Address,State,City,Pincode) 
+values(1,"Jitendra","Patil","Male","50.0","5.9","2000-10-07","9893063933","9753400020","jatin@gmail.com","Jamna nagar p-80","Madhya Pradesh","Burhanpur",450331);
+select * from Patients;
 
 desc Patients;
-
-
 
 create table Doctors(
   DocId int not null primary key auto_increment,
@@ -64,10 +63,15 @@ create table Doctors(
   Pincode integer(6),
   foreign key (UsrId) references Users(UsrId) on delete cascade on update cascade
 );
+insert into Doctors (UsrId,First_name,Last_name,DOB,IsActive,Gender,Doctor_Type,Home_Phone,Emrgncy_phone,Emrgncy_email,Address,State,City,Pincode) 
+values(2,"Lokesh","Patel","2002-02-19",1,"Male","Neurologist","8766433255","7896541233","lokesh@gmail.com","Aryanagar p-01","Maharastra","Nagpur",440026);
+
+select * from Doctors;
 
 
 
 desc Doctors;
+
 
 create table Admin(
   AdmnId int not null primary key auto_increment,
@@ -82,6 +86,11 @@ create table Admin(
   Pincode integer(6) not null,
   foreign key(UsrId) references Users(UsrId) on delete cascade on update cascade
 );
+
+insert into Admin(UsrId,First_name, Last_name, Mobile, Email, Address, State, City, Pincode)
+ values(3,"Dhanraj","Jadhav", "9212173314", "dhanraj@gmail.com", "Sangli p-70", "Maharastra", "sangli", "440034");
+ select * from Admin;
+ -- delete from Admin where AdmnId = 1;
 
 desc Admin;
 
@@ -98,6 +107,10 @@ create table Appointment(
   foreign key(DocId) references Doctors(DocId) on delete cascade on update cascade
 );
 
+insert into Appointment (PtntId, DocId, Apptmnt_Date, Apptmnt_Status, Apptmnt_Type, Apptmnt_for_Dscription) 
+values (1,1,curdate(), "Booked", "Check_Up","Regular check up");
+select * from Appointment;
+
 desc Appointment;
 
 create table Priscription(
@@ -107,6 +120,11 @@ create table Priscription(
   Validity_end_date date,
   foreign key (ApptmntId) references Appointment(ApptmntId) on delete cascade on update cascade
 );
+
+insert into Priscription(ApptmntId, Validity_start_date, Validity_end_date)
+values (1, curdate(), "2024-01-05");
+
+select * from Priscription;
 
 desc Priscription;
 
@@ -120,6 +138,11 @@ create table Medication_Product(
   foreign key(Prscrptn_number) references Priscription(Prscrptn_number) on delete cascade on update cascade
 );
 
+insert into Medication_Product(Prscrptn_number, Medicin_Name, Days_supply, Dosages_Instructions, Quantity, Medicin_price)
+values (1, "Dolo", 4, "take it 3 times a day , Don't skip it..", 12, 120.79);
+
+select * from Medication_Product;
+
 desc Medication_Product;
 
 create table Feedback(
@@ -130,7 +153,11 @@ create table Feedback(
   foreign key (PtntId) references Patients(PtntId) on delete cascade on update cascade,
   foreign key(DocId) references Doctors(DocId) on delete cascade on update cascade
 );
- 
+
+
+insert into Feedback(PtntId, DocId, Fdbck, IsSatisfied) values (1, 1, "Great doctor, I will advice more people to visit here..", "Fully_Satisfied");
+select * from Feedback;
+
 desc Feedback;
 
 create table Tests(
@@ -142,6 +169,9 @@ create table Tests(
   foreign key (AdmnId) references Admin(AdmnId) on delete cascade on update cascade
 );
 alter table Tests modify Test_type ENUM('Blood_test','Suger_test','X_ray','MRI','Bone_marrow_test') not null;
+
+
+insert into Tests(ApptmntId, Test_type, AdmnId) values (1, "Blood_test", 1);
 
 desc Tests;
 
@@ -159,6 +189,9 @@ create table Billings(
   foreign key(AdmnId) references Admin(AdmnId) on delete cascade on update cascade
 );
 
+
 desc Billings;
+
+
 
 
